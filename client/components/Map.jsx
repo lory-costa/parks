f76e3caa47272a0114fd7e5e3f67d8d3b4e90835
Sarah Smith <77254910+sarah-smith-nz@users.noreply.github.com>
2021-05-06 11:11:06 +1200

    add map, header and nav components with links

---
+++ client/components/Map.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+// hard coded data to be used for position
+// const lat = [-36.877796186450325] 
+// const lon = [174.78603054886526]
+
+export default function Map ({ addresses, coordinates }) {
+  return (
+    <div className='column'>
+      <MapContainer
+        center={[-36.8666700, 174.7666700]}
+        zoom={11}
+        scrollWheelZoom={true}>
+        <TileLayer
+          url='https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWljaGFlbC1yIiwiYSI6ImNrZXM1Zm9iaDJiNmYycW1za2dobDZ4d3gifQ.a5mK2DxNqWhlzvoa8Zxb2Q'/>
+        {coordinates.map((location, i) => {
+          return <Marker key={i}
+            position={[location.lat, location.lon]}
+          >
+            <Popup>
+              {addresses[i]}
+            </Popup>
+          </Marker>
+        })}
+      </MapContainer>
+    </div>
+  )
+}

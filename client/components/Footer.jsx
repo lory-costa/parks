05050b337e3e710c920839b3d44c6d63ee0a22ab
Lorena Costa <loraphaela@gmail.com>
2021-05-06 22:03:50 +1200

    update map and main pages

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom'
 
 function Footer () {
   return (
-    <div className='flex justify-between mt-8 mx-14 absolute inset-x-0 bottom-2 '>
+    <div className='flex justify-between mt-8 mx-14 absolute inset-x-0 bottom-4 text-green-700'>
+      {/* THIS COULD BE THE 'ABOUT'? */}
       <div className='py-2 px-4'>
-      <Link to="/">Kahikatea Dev Academy</Link>
+        <Link to="/">Kahikatea Dev Academy</Link>
       </div>
+      {/* DONT NEED TO MAKE THIS SO BIG? */}
       <div className='py-2 px-4'>
-      <Link to="/">About</Link>
+        <Link to="/">Suggest a Park</Link>
       </div>
     </div>
   )

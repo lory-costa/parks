5e007f63421085f9bc744d898e2dd70eac9f18d2
Sina Kakhki <78020532+sina-kakhki@users.noreply.github.com>
2021-05-06 16:31:15 +1200

    initial routes setup

--- server/db/seeds/parkComment.js
+++ server/db/seeds/parkComment.js
@@ -2,8 +2,8 @@ exports.seed = function (knex) {
   return knex('parkComment').del()
     .then(() =>
       knex('parkComment').insert([
-        { id: 1, user_id: '1', park_id: '1', comment: 'Nice Park!' },
-        { id: 2, user_id: '2', park_id: '2', comment: 'Awesome!' },
+        { id: 1, user_id: '1', park_id: '1', comment: 'Nice Park! Omg my kids love it' },
+        { id: 2, user_id: '2', park_id: '2', comment: 'Awesome! My wife loves it' },
         { id: 3, user_id: '2', park_id: '3', comment: 'Crappy place.' },
         { id: 4, user_id: '1', park_id: '4', comment: 'Very green.' }
       ])

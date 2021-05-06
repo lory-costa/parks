e0a7395051f2e31224cb4d81db91bb68f763f394
Lorena Costa <loraphaela@gmail.com>
2021-05-06 16:43:17 +1200

    add margin bottom to paragraph

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -6,8 +6,8 @@ function Main () {
   return (
     <div className='flex flex-col'>
       <Header />
-      <div className='mt-20 flex justify-center' >
-        <p className='text-xl' >Select a park for details</p>
+      <div className='mt-20 mb-8 flex justify-center' >
+        <p className='text-xl'>Select a park for details</p>
       </div>
       <Map />
     </div>

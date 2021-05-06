bfa01a099707bff0b5f27f94062f24a2198d80e3
johannbesas <johannbesas@gmail.com>
2021-05-07 11:23:22 +1200

    updated nav and auth0 buttons

--- client/components/LoginButton.jsx
+++ client/components/LoginButton.jsx
@@ -4,7 +4,9 @@ import { useAuth0 } from '@auth0/auth0-react'
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0()
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>
+  return <button className='bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded' onClick={() => loginWithRedirect()}>
+    Log In
+  </button>
 }
 
 export default LoginButton

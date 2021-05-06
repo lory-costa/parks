a8713a5dbf00fbb2a15f8c7a23dd56b83b42c42c
sina <ksina.72@gmail.com>
2021-05-07 10:58:11 +1200

    working on comment rout

--- server/db/parkComment.js
+++ server/db/parkComment.js
@@ -2,8 +2,8 @@ const connection = require('./connection')
 
 module.exports = {
   addComment,
-  deleteComment
-  // getCommentById,
+  deleteComment,
+  getCommentsByParkId
   // updateComment
 
 }
@@ -20,37 +20,37 @@ function deleteComment (submission, db = connection) {
     .delete()
 }
 
-// function getCommentById (id, db = connection) {
-//   return db('parkComment')
-//     .where('parkComment.id', id)
-//     .leftJoin('users', 'parkComment.user_id', 'users.id')
-//     .leftJoin('parks', 'parkComment.park_id', 'parks.id')
-//     .select(
-//       'name',
-//       'user_id',
-//       'park_id',
-//       'parkComment.id as id',
-//       'parkComment.park_id as parkId',
-//       'parkComment.comment',
-//       'user_id as userId',
-//       'username',
-//       'first_name',
-//       'last_name'
-//     )
-//     .then(result => {
-//       const comment = result[0]
-//       return {
-//         id: comment.id,
-//         parkId: comment.parkId,
-//         comments: !park.parkCommentId ? [] : result.map((comment) => {
-//           return {
-//             id: comment.commentId,
-//             comment: comment.comment
-//           }
-//         })
-//       }
-//     })
-// }
+function getCommentsByParkId (id, db = connection) {
+  return db('parkComment')
+    .where('parkComment.park_id', id)
+    .leftJoin('users', 'parkComment.user_id', 'users.id')
+    .leftJoin('parks', 'parkComment.park_id', 'parks.id')
+    .select(
+      'name',
+      'user_id',
+      'park_id',
+      'parkComment.id as id',
+      'parkComment.park_id as parkId',
+      'parkComment.comment',
+      'user_id as userId',
+      'username',
+      'first_name',
+      'last_name'
+    )
+    .then(result => {
+      const comment = result[0]
+      return {
+        id: comment.id,
+        parkId: comment.parkId,
+        comments: !park.parkCommentId ? [] : result.map((comment) => {
+          return {
+            id: comment.commentId,
+            comment: comment.comment
+          }
+        })
+      }
+    })
+}
 
 // function updateComment (updatedEvent, db = connection) {
 //   const { id, comment } = updatedComment
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const parkRoutes = require('./routes/parks')
+const commentRoutes = require('./routes/comments')
 
 const server = express()
 
@@ -9,5 +10,6 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/park', parkRoutes)
+server.use('/api/v1/comments', commentRoutes)
 
 module.exports = server

4a61bcc6cccd8335f109478e9c02f4b5fab5eb47
Sarah Smith <77254910+sarah-smith-nz@users.noreply.github.com>
2021-05-07 17:32:50 +1200

    facilities icons showing

--- client/components/Facilities.jsx
+++ client/components/Facilities.jsx
@@ -7,19 +7,18 @@ function Facilities (props) {
   const sports = props.sportsField
   const tramp = props.tramp
   const dogs = props.dogWalking
-  console.log(props.url)
+  console.log(props)
   return (
     <div>
       <h3>Facilities</h3>
 
       {playground && <img src='/icons/playground.svg.png' alt="playground icon" width="50" height="50"/>}
       {toilets && <img src='/icons/icon-toilets.svg' alt="toilet icon" width="50" height="50"/>}
-      {picnic && <img src='/icons/picnic-area.svg' alt="picnic icon" width="50" height="50"/> }
-      {sports && <img src='/icons/icons-sports.svg' alt="sports icon" width="50" height="50"/> }
+      {picnic && <img src='/icons/icon-picnic-area.svg' alt="picnic icon" width="50" height="50"/> }
+      {sports && <img src='/icons/icon-sports-field.svg' alt="sports icon" width="50" height="50"/> }
       {tramp && <img src='/icons/icon-walking.svg' alt="tramp walking icon" width="50" height="50"/>}
 
-      {!dogs ? <img src='/icons/dogAllowed.png' alt="dog allowed icon" width="50" height="50"/> : <img src='/icons/dogNotAllowed.png' alt="dog not
-      allowed icon" width="50" height="50"/>}
+      {dogs && <img src='/icons/dogAllowed.png' alt="dog allowed icon" width="50" height="50"/>}
 
       <p>More information:{props.url}</p>
     </div>

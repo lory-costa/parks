030feefbfd907c18c87e1097c187f4c63368ee27
Lorena Costa <loraphaela@gmail.com>
2021-05-07 21:53:16 +1200

    improve responsiveness

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -7,11 +7,11 @@ export default function Nav () {
   const { isAuthenticated } = useAuth0()
 
   return (
-    <div className='flex w-48 justify-between'>
+    <div className='flex justify-between'>
       <div>
         {!isAuthenticated ? <LoginButton /> : < LogoutButton />}
       </div>
-      <button className='bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded'>Button</button>
+      <button className='bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded ml-4'>Button</button>
     </div>
   )
 }

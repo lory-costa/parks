7a5d8f60c071b93e9b144658b9f4271631e267b6
jonnathan <root@Ignis-PC.localdomain>
2021-05-09 11:23:05 +1200

    green marker implemented

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
+const greenIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+})
+
 export default function Map ({ ids, addresses, coordinates, names, images }) {
   return (
     <MapContainer className="mt-5" style={{ width: '100vw', height: 'calc(100vh - 172px)' }}
@@ -11,14 +20,14 @@ export default function Map ({ ids, addresses, coordinates, names, images }) {
       <TileLayer
         url='https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWljaGFlbC1yIiwiYSI6ImNrZXM1Zm9iaDJiNmYycW1za2dobDZ4d3gifQ.a5mK2DxNqWhlzvoa8Zxb2Q' />
       {coordinates.map((location, i) => {
-        return <Marker key={i}
+        return <Marker className='greenIcon' key={i}
+          icon={greenIcon}
           position={[location.lat, location.lon]}
         >
           <Popup>
             <div><Link to={`/park-details/${ids[i]}`}>{names[i]}</Link></div>
             <div>{addresses[i]}</div>
             <img src={images[i]}></img>
-            {/* <Link to={`/park-details/${ids[i]}`}>{parkName[i]}</Link> */}
           </Popup>
         </Marker>
       })}

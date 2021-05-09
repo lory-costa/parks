e849e89e235ccb21844e32fc17210a9031b806a6
johannbesas <johannbesas@gmail.com>
2021-05-09 20:56:05 +1200

    tidy up with filter, map dimensions

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -9,7 +9,7 @@ function Header () {
     <div className='flex justify-between mt-8 mx-14 items-center' >
       <div className='text-4xl' >
         <Link to="/main">
-          <img className='flex flex-col bg-cover' src='/images/logos/ProjectLogoFinal.png'/>
+          <img className='flex flex-col bg-cover h-20' src='/images/logos/ProjectLogoFinal.png'/>
         </Link>
       </div>
       <Nav />

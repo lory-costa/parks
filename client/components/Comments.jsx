8382b0fcc09da629f3e00d62983eae1006144251
johannbesas <johannbesas@gmail.com>
2021-05-09 20:19:34 +1200

    suggest park only shows for logged in users

--- client/components/Comments.jsx
+++ client/components/Comments.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addComment, fetchComments } from '../actions/comments'
+import { useAuth0 } from '@auth0/auth0-react'
 
 import CommentItem from './CommentItem'
 import Rating from './Rating'
@@ -11,6 +12,7 @@ function Comments (props) {
   const parkId = props.parkId
   const [newComment, setNewComment] = useState('')
   const dispatch = useDispatch()
+  const { isAuthenticated } = useAuth0()
 
   useEffect(() => {
     dispatch(fetchComments(parkId))
@@ -29,13 +31,14 @@ function Comments (props) {
   return (
     <div className='mt-8 mx-14'>
       <h3 className='text-xl mb-4 text-green-700'>Comments</h3>
-      <div className='bg-gray-100 mb-4 p-4 rounded' >
+      {isAuthenticated && <div className='bg-gray-100 mb-4 p-4 rounded' >
         <Rating />
         <div className='flex flex-col lg:flex-row justify-between items-center mt-2' >
           <input className='border rounded py-2 px-4 w-full' type="text" placeholder="Add a comment" value={newComment} onChange={handleChange} />
           <button className='border rounded py-2 px-4 mt-2 ml-0 lg:ml-2 lg:mt-0 bg-white' onClick={handleSubmit}>Submit</button>
         </div>
       </div>
+      }
       <ul>
         {comments.map(comment => (
           <CommentItem key={comment.id} userComment={comment} />
--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function Footer () {
+  const { isAuthenticated } = useAuth0()
   const isAdmin = useSelector(globalState => globalState.user.isAdmin)
 
   return (
@@ -11,10 +13,11 @@ function Footer () {
         <Link to="/">Kahikatea Dev Academy</Link>
       </div>
       <div className='py-2'>
-        <Link to="/add-park">
-          {isAdmin ? 'Add a Park' : 'Suggest a Park' }
-        </Link>
+        { isAuthenticated && <Link to="/add-park">
+          {isAdmin ? 'Add a Park' : 'Suggest a Park'}
+        </Link>}
       </div>
+
     </div>
   )
 }

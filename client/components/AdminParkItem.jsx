9ebfbeeb0266da93d43afe16a47b6414a10c307e
johannbesas <johannbesas@gmail.com>
2021-05-11 11:12:50 +1200

    icons replace checkbox to toggle approval

--- client/components/AdminParkItem.jsx
+++ client/components/AdminParkItem.jsx
@@ -13,23 +13,27 @@ export default function AdminParkItem ({ parkListing, deleteItem }) {
     return toggleParkApprovedStatus(id, !isApprovedStatus)
   }
 
-  return(
-    
+  function toggleClick () {
+    setIsApproved(!isApprovedStatus)
+    return toggleParkApprovedStatus(id, !isApprovedStatus)
+  }
+
+  return (
+
     <div
-    className='border-gray-200 border-2 rounded-lg'
-    key={id}>
+      className='border-gray-200 border-2 rounded-lg'
+      key={id}>
       <Link to={`/edit-park/${id}`}>
         <img src={parkListing.image} alt="park image" className='object-cover h-36 w-full rounded-t-lg mb-2' />
       </Link>
       <p className='ml-2'>{name}</p>
       <div className='flex justify-between ml-2 items-center' >
-      <img src='/images/active.gif' alt="Remove Park" width='30'/>
-        <input
-        type="checkbox" 
-        checked={isApprovedStatus} 
-        onChange={(event) => handleInputChange(event)} />
+        <button onClick={toggleClick}>
+          {isApprovedStatus ? <img src='/icons/activeMarker.gif' alt="Active Park" width='30' />
+            : <img src='/icons/dormantMarker.png' alt="Dormant Park" width='30' />}
+        </button>
         <button onClick={() => { if (window.confirm('Are you sure you wish to delete this park?')) deleteItem(id) }}>
-        <img src='/images/trash.png' alt="Remove Park" width='30'/>
+          <img src='/images/trash.png' alt="Remove Park" width='30' />
         </button>
       </div>
     </div>

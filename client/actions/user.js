1f7cf594c446ab32a6183f0e2b408f6e1419d344
johannbesas <johannbesas@gmail.com>
2021-05-10 14:28:45 +1200

    implemented logic to handle unverified users

--- client/actions/user.js
+++ client/actions/user.js
@@ -2,7 +2,8 @@ export const SET_USER = 'SET_USER'
 export const CLEAR_USER = 'CLEAR_USER'
 
 export function setUser (user) {
-  const isAdminValue = user['https://parkwise/roles'][0] === 'Admin'
+  const role = user['https://parkwise/roles'] || false
+  const isAdminValue = role && role[0] === 'Admin'
   return {
     type: SET_USER,
     user: {

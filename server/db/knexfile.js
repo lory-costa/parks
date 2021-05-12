7df7b5c9ed0a573fb3db2ee2c083e565573c3cbf
Sina Kakhki <78020532+sina-kakhki@users.noreply.github.com>
2021-05-12 16:51:05 +1200

    using git for heroku deployment

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,6 +1,14 @@
 const path = require('path')
+require('dotenv').config({ path: 'path_to_env_file' })
 
 module.exports = {
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  },
   development: {
     client: 'sqlite3',
     connection: {

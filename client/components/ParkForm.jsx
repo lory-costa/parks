d2c806b45da799fee85d426874c0eea1bc500997
Lorena Costa <loraphaela@gmail.com>
2021-05-20 13:13:27 +1200

    add confirmation of park suggested

--- client/components/ParkForm.jsx
+++ client/components/ParkForm.jsx
@@ -1,72 +1,79 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { validate, rules } from './ValidationForm'
-import ParkFormFacilityItem from './ParkFormFacilityItem'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { validate, rules } from "./ValidationForm";
+import ParkFormFacilityItem from "./ParkFormFacilityItem";
 
-export default function ParkForm (props) {
-  const isAdmin = useSelector((globalState) => globalState.user.isAdmin)
-  const [widget, setWidget] = useState({})
+export default function ParkForm(props) {
+  const isAdmin = useSelector((globalState) => globalState.user.isAdmin);
+  const [widget, setWidget] = useState({});
 
   // state for image upload button
-  const [color, setColor] = useState('bg-gray-700 hover:bg-gray-500 text-white py-2 px-4 rounded focus:outline-green-500')
-  const [upload, setUpload] = useState('Upload Photo')
-  const [image, setImage] = useState((props.formData && props.formData.image) || '')
-  const [invalid, setInvalid] = useState({})
+  const [color, setColor] = useState(
+    "bg-gray-700 hover:bg-gray-500 text-white py-2 px-4 rounded focus:outline-green-500"
+  );
+  const [upload, setUpload] = useState("Upload Photo");
+  const [image, setImage] = useState(
+    (props.formData && props.formData.image) || ""
+  );
+  const [invalid, setInvalid] = useState({});
   const [form, setForm] = useState(
     props.formData || {
-      name: 'Albert Park',
-      address: '33-43 Princes Street',
+      name: "Albert Park",
+      address: "33-43 Princes Street",
       lat: -36.85069409130585,
       lon: 174.76748040205254,
-      url: 'https://www.aucklandcouncil.govt.nz/parks-recreation/Pages/park-details.aspx?Location=169',
-      description: 'Albert Park is one of Auckland’s most important parks. Its central location in the heart of the CBD, together with its long history and distinctive character, have earned it a special place in the hearts of Aucklanders and made it an important destination for visitors.',
+      url:
+        "https://www.aucklandcouncil.govt.nz/parks-recreation/Pages/park-details.aspx?Location=169",
+      description:
+        "Albert Park is one of Auckland’s most important parks. Its central location in the heart of the CBD, together with its long history and distinctive character, have earned it a special place in the hearts of Aucklanders and made it an important destination for visitors.",
       playGround: 0,
       toilets: 1,
       picnicSite: 1,
       sportsField: 1,
       tramp: 1,
       dogWalking: 1,
-      approved: 0
+      approved: 0,
     }
-  )
+  );
   // Validation for required fields
   const validationRules = {
     name: [rules.isRequired],
     address: [rules.isRequired],
     url: [rules.isRequired],
-    description: [rules.isRequired]
-  }
+    description: [rules.isRequired],
+  };
 
   // onChange for text inputs
-  function handleChange (e) {
-    const { name, value } = e.target
+  function handleChange(e) {
+    const { name, value } = e.target;
     setForm({
       ...form,
-      [name]: value
-    })
+      [name]: value,
+    });
   }
   // onChange for checkbox inputs
-  function handleInputChange (event) {
-    const target = event.target
-    const name = target.name
+  function handleInputChange(event) {
+    const target = event.target;
+    const name = target.name;
 
     setForm({
       ...form,
-      [name]: target.checked
-    })
+      [name]: target.checked,
+    });
   }
 
   // Submit
-  function handleSubmit (e) {
-    const results = validate(form, validationRules, invalid)
-    e.preventDefault()
+  function handleSubmit(e) {
+    const results = validate(form, validationRules, invalid);
+    e.preventDefault();
 
     if (results.isValid) {
-      const park = { ...form, image }
-      props.submitPark(park)
+      const park = { ...form, image };
+      props.submitPark(park);
+      alert("Park submitted for approval");
     } else {
-      console.log('something went wrong')
-      setInvalid(results.details)
+      console.log("something went wrong");
+      setInvalid(results.details);
     }
   }
 
@@ -83,43 +90,47 @@ export default function ParkForm (props) {
     sportsField,
     tramp,
     dogWalking,
-    approved
-  } = form
+    approved,
+  } = form;
 
-  function changeButtonColor () {
-    setColor('bg-green-700 text-white py-2 px-4 rounded focus:outline-none')
+  function changeButtonColor() {
+    setColor("bg-green-700 text-white py-2 px-4 rounded focus:outline-none");
   }
 
   // Cloudinary functions
   // check upload was successful then pull the sescure_url off the response
   // NOTE: image is secure_url
   const checkUploadResult = (resultEvent) => {
-    if (resultEvent.event === 'success') {
-      changeButtonColor()
-      setUpload('Image uploaded')
-      setImage(resultEvent.info.secure_url)
-      console.log(resultEvent.info.secure_url)
+    if (resultEvent.event === "success") {
+      changeButtonColor();
+      setUpload("Image uploaded");
+      setImage(resultEvent.info.secure_url);
+      console.log(resultEvent.info.secure_url);
     }
-  }
+  };
   // pop up widget shows for image upload
-  function showWidget (event, widget) {
-    event.preventDefault()
-    widget.open()
+  function showWidget(event, widget) {
+    event.preventDefault();
+    widget.open();
   }
   // Accessing cloudinary account in widget pop-up
   useEffect(() => {
-    setWidget(window.cloudinary.createUploadWidget({
-      cloudName: 'dvsikj1gh',
-      uploadPreset: 'guboz3wj'
-    },
-    (error, result) => {
-      if (error) {
-        // TODO: Let the user know something is wrong
-        return
-      }
-      checkUploadResult(result)
-    }))
-  }, [])
+    setWidget(
+      window.cloudinary.createUploadWidget(
+        {
+          cloudName: "dvsikj1gh",
+          uploadPreset: "guboz3wj",
+        },
+        (error, result) => {
+          if (error) {
+            // TODO: Let the user know something is wrong
+            return;
+          }
+          checkUploadResult(result);
+        }
+      )
+    );
+  }, []);
 
   return (
     <>
@@ -156,7 +167,9 @@ export default function ParkForm (props) {
               value={address}
               onChange={handleChange}
             />
-            {invalid.address && <div className='text-red-500'>{invalid.address}</div>}
+            {invalid.address && (
+              <div className='text-red-500'>{invalid.address}</div>
+            )}
           </div>
 
           <div className='mt-4'>
@@ -223,61 +236,69 @@ export default function ParkForm (props) {
               value={description}
               onChange={handleChange}
             />
-            {invalid.description && <div className='text-red-500' >{invalid.description}</div>}
+            {invalid.description && (
+              <div className='text-red-500'>{invalid.description}</div>
+            )}
           </div>
 
           <div className='mt-4'>
             <label htmlFor='image' className='text-lg'>
-            Image
+              Image
             </label>
             <div id='photo-form-container'>
-              <button className={color} onClick={(event) => { showWidget(event, widget) }}>{upload}</button>
+              <button
+                className={color}
+                onClick={(event) => {
+                  showWidget(event, widget);
+                }}
+              >
+                {upload}
+              </button>
             </div>
           </div>
-
         </div>
 
         <div className='mt-4'>
           <ParkFormFacilityItem
-            facilityName={'Playground'}
-            facilityValue={'playGround'}
+            facilityName={"Playground"}
+            facilityValue={"playGround"}
             checkValue={playGround}
             onChangeFunc={handleInputChange}
           />
           <ParkFormFacilityItem
-            facilityName={'Toilets'}
-            facilityValue={'toilets'}
+            facilityName={"Toilets"}
+            facilityValue={"toilets"}
             checkValue={toilets}
             onChangeFunc={handleInputChange}
           />
           <ParkFormFacilityItem
-            facilityName={'Picnic Site'}
-            facilityValue={'picnicSite'}
+            facilityName={"Picnic Site"}
+            facilityValue={"picnicSite"}
             checkValue={picnicSite}
             onChangeFunc={handleInputChange}
           />
           <ParkFormFacilityItem
-            facilityName={'Sports Field'}
-            facilityValue={'sportsField'}
+            facilityName={"Sports Field"}
+            facilityValue={"sportsField"}
             checkValue={sportsField}
             onChangeFunc={handleInputChange}
           />
           <ParkFormFacilityItem
-            facilityName={'Tramping'}
-            facilityValue={'tramp'}
+            facilityName={"Tramping"}
+            facilityValue={"tramp"}
             checkValue={tramp}
             onChangeFunc={handleInputChange}
           />
           <ParkFormFacilityItem
-            facilityName={'Dog Walking Allowed'}
-            facilityValue={'dogWalking'}
+            facilityName={"Dog Walking Allowed"}
+            facilityValue={"dogWalking"}
             checkValue={dogWalking}
             onChangeFunc={handleInputChange}
           />
         </div>
       </form>
 
-      <div className='flex flex-col md:flex-row justify-between lg:w-1/3 mt-8 mb-8' >
+      <div className='flex flex-col md:flex-row justify-between lg:w-1/3 mt-8 mb-8'>
         <button
           className='bg-green-700 hover:bg-green-500 text-white py-2 px-4 rounded focus:outline-none'
           onClick={handleSubmit}
@@ -285,10 +306,21 @@ export default function ParkForm (props) {
           {props.action}
         </button>
         {isAdmin && (
-          <div className='flex flex-row justify-between items-center' >
+          <div className='flex flex-row justify-between items-center'>
             <div>
-              {approved ? <img src='/icons/activeMarker.gif' alt="Active Park" width='20' />
-                : <img src='/icons/dormantMarker.png' alt="Dormant Park" width='20' />}
+              {approved ? (
+                <img
+                  src='/icons/activeMarker.gif'
+                  alt='Active Park'
+                  width='20'
+                />
+              ) : (
+                <img
+                  src='/icons/dormantMarker.png'
+                  alt='Dormant Park'
+                  width='20'
+                />
+              )}
             </div>
             <div className='mx-4'>
               <input
@@ -308,5 +340,5 @@ export default function ParkForm (props) {
         )}
       </div>
     </>
-  )
+  );
 }
